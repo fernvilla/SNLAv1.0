c3582a10f1cbeb28ab06755cad53b5a9a339de6e
fernvilla <fvilla18@gmail.com>
2014-04-06 11:25:38 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SportsNewsLA::Application.config.secret_key_base = '50e0d2507b5292a8d572f11beb0494e5a30aa2138350b944f9db650dd9eef4585e9d183435962540063f86dc3b876d8f4b166b91cd21fbf612b9772a804fbeef'
